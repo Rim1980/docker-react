45c851c5216a1719c1f24f5809a32d828507ebf9
Rim1980 <50277447+Rim1980@users.noreply.github.com>
2019-05-27 23:36:25 -0400

    update to branch

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Reem Noura Edit <code>src/App.js</code> and save to reload.
+          Reem Noura Jenna Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
